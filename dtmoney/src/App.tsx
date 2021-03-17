e3cc6ca61dc27f267f0a20dc18404133da432e0f
Wescley Torres <jewescley@gmail.com>
2021-03-17 16:23:00 -0300

    Chapter II 2/5 - Complete 🎉

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,10 +1,13 @@
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
     --blue-light: #6933ff;
     --text-title: #363f5f;
     --text-body: #969cb3;
@@ -28,7 +29,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
