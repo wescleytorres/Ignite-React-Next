a18c81152273772bafee7fa17b02a8fd8e4baf90
Wescley Torres <jewescley@gmail.com>
2021-05-17 21:10:01 -0300

    Desafios

---
+++ Desafios/Ignite-React-Desafio02/src/components/Content.tsx
@@ -0,0 +1,27 @@
+import { MovieProps } from "../types";
+import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: string;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ Desafios/Ignite-React-Desafio02/src/components/SideBar.tsx
@@ -0,0 +1,33 @@
+import { GenreResponseProps } from "../types";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: SideBarProps) {
+
+  return (
+
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+}
